800e95541a075e4a3b139b5b1625888fe11464d0
Casey <hjkdw9506@gmail.com>
2021-08-03 21:29:48 +0900

    🎨 Taking Turns까지 완료

--- src/index.js
+++ src/index.js
@@ -2,35 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-    // add a constructor to the class to initialize the state:
-    constructor(props) {
-        super(props);    //  you need to always call super when defining the constructor of a subclass. 
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => { this.setState({value: "X"}) }}
-        >
-          {this.state.value}
-        </button>
-      );
-    }
+// In React, function components are a simpler way to write components that only contain a render method and don’t have their own state. Instead of defining a class which extends React.Component, we can write a function that takes props as input and returns what should be rendered. Function components are less tedious to write than classes, and many components can be expressed this way.
+  function Square(props) {
+    return (
+      <button className="square" onClick={() => { props.onClick() }}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
   
       return (
         <div>
